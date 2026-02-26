e560877d61ec010d78380f34d1347010117fab1f
modalaiso <mobioisrael@hotmail.com>
2026-02-26 20:20:06 +0000

    Remove index.html and add node_modules
    Delete index.html and add a complete node_modules tree (Express and many dependencies such as body-parser, qs, mime-types, negotiator, etc.). Changes appear to come from installing project dependencies (npm install), adding package files, libs, READMEs and licenses; no other source files modified.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// dossier contenant tes fichiers HTML
+const publicDir = path.join(__dirname, 'public');
+app.use(express.static(publicDir));
+
+// réécriture pour les fichiers .html
+app.get('/:name', (req, res) => {
+  const file = path.join(publicDir, req.params.name + '.html');
+  res.sendFile(file, err => {
+    if (err) res.status(404).send('Page non trouvée');
+  });
+});
+
+app.listen(3000, () => console.log('Serveur lancé sur http://localhost:3000'));
